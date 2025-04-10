13817a292cad16044d0b560ff30d2271a58fab6b
IsabelArriaga <a01722621@itesm.mx>
2025-04-10 09:58:09 -0400

    Level 5 Complete

--- python_review.py
+++ python_review.py
@@ -15,3 +15,41 @@ age = 32 # The variable called age is now holding the integer value 32
 name = "Insert your name here" # The variable called name is now holding the string value "Insert your name here"
 instrument = "Insert your instrument here" # The variable called instrument is now holding the string value "Insert your instrument here" (both of these could be great for an input value! aha !)
 
+# The input from the user is valuable so you wanna have user put it in only once 
+
+# prompt = "Enter your name"
+# response = input(prompt)
+# print("Hello,", response)
+# print(response, "here is your agenda for the day...")
+
+# userName = "Enter your name "
+# nameResponse = input(userName)
+# userAge = "Enter your age "
+# ageResponse = input(userAge)
+# print("Hello, ", nameResponse, "aged", ageResponse, "whose datatypes are", type(nameResponse), "and ", type(ageResponse))
+
+# #Ask user for two numbers
+# first_number = input("What's the first number?")
+# second_number = input("What's the second number?")
+
+# #Convert numbers to integer versions of themselves
+# first_number = int(first_number)
+# second_number = int(second_number)
+
+# #Compare the two numbers
+# if first_number > second_number:
+#     print(first_number, "is bigger")
+# elif first_number == second_number:
+#     print("the numbers are equal!!!")
+# else:
+#     print(second_number, "is bigger")
+
+name = input("What's your full name?")
+length_of_name = len(name)
+print(length_of_name)
+
+while " " in name:
+    name = name.replace(" ", "")
+
+length_of_name = len(name)
+print(length_of_name)
