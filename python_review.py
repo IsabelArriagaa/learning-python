81418b89d5d413e5abfad5b2b6ed8902a47df371
IsabelArriaga <a01722621@itesm.mx>
2025-04-10 09:15:33 -0400

    Level 4 Complete

--- python_review.py
+++ python_review.py
@@ -3,5 +3,15 @@ print ("It worked!")
 
 # x = none --> to create abyss 
 x = 3
-
+# The variable called [insert variable name here] is now holding the [data type] value [value].
+example = "I'm a string" # The variabel called example is now holding the string value "I´m a string!"
+a = 3 # The variable called a is now holding the integer value 3
+b = 4.0 # The variable called b is now holding the flaot value 4.0
+c = True # The variable called c is now holding the boolean value True
+d = False # The variable called d is now holding the boolean value False
+e = "Hey!" # The variable called e is now holding the string value "Hey!"
+f = None # The variable called f is now holding the abyss (absence of a) value None
+age = 32 # The variable called age is now holding the integer value 32
+name = "Insert your name here" # The variable called name is now holding the string value "Insert your name here"
+instrument = "Insert your instrument here" # The variable called instrument is now holding the string value "Insert your instrument here" (both of these could be great for an input value! aha !)
 
